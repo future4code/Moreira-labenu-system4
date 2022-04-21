4c8c2733dc7ebd23c28acba5c9b5087286079a76
Claudia Carvalho <60494846+claudia-carvalho@users.noreply.github.com>
2022-04-21 08:39:36 -0300

    atualizado endpoint

--- src/endpoints/buscarDocente.ts
+++ src/endpoints/buscarDocente.ts
@@ -4,10 +4,8 @@ import { docente } from "../types/types"
 
 export default async function buscarDocente(req: Request, res: Response): Promise<void> {
   try {
-
     const docentes: docente[] = await connection("Docente")
     res.send(docentes)
-
   } catch (error: any) {
     res.status(500).send(error.sqlMessage)
   }
