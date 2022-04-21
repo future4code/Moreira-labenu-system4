0933e4f62d24447164ba8059e694462f7d46ecf7
Claudia Carvalho <60494846+claudia-carvalho@users.noreply.github.com>
2022-04-21 06:47:16 -0300

    criado endpoint de criar docente

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import app from "./app";
 import criarEstudante from "./endpoints/criarEstudante";
 import buscarEstudante from "./endpoints/buscarEstudante";
 import criarTurma from "./endpoints/criarTurma";
+import criarDocente from "./endpoints/criarDocente";
 
 //criar turma
-app.post('/turma/criar', criarTurma )
+app.post('/turma/criar', criarTurma)
 
 //get estudantes
 app.get("/estudante/:nome", buscarEstudante);
@@ -13,3 +14,7 @@ app.get("/estudante/:nome", buscarEstudante);
 app.post("/criarEstudante", criarEstudante);
 
 //put turma estudante
+
+
+//criar docente
+app.post("/docente", criarDocente)
