a1828602d763c1336052694db8110cd68a6a92fc
Carlise Debona <92445126+Carlisegd@users.noreply.github.com>
2022-04-22 11:28:46 -0300

    buscar turmas, iniciando o editar turma

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,17 @@ import buscarEstudante from "./endpoints/buscarEstudante";
 import criarTurma from "./endpoints/criarTurma";
 import criarDocente from "./endpoints/criarDocente";
 import editarTurmaEstudante from "./endpoints/editarEstudante";
-
+import buscarTurma from "./endpoints/buscarTurma";
+//import mudarTurma from "./endpoints/editarTurma";
 
 //criar turma
 app.post('/turma/criar', criarTurma)
+
+//buscar turma
+app.get('/turma', buscarTurma)
+
+//buscar turma
+//app.put('/turma/:id', mudarTurma)
 
 //get estudantes
 app.get("/estudante/:nome", buscarEstudante);
